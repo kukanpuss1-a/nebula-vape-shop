b3e5e6280061d065ddadcdd324eca5bdcf7fe782
kukanpuss1-a <konstantyndolyna@gmail.com>
2024-09-17 19:03:00 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+// Ваш токен бота
+const token = process.env.TELEGRAM_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+// Запускаем Express сервер для обслуживания веб-приложения
+app.use(express.static('public'));
+app.listen(3001, () => {
+    console.log('Web app is running on http://localhost:3001');
+});
+
+// Обработка команды /start и создание кнопки для открытия веб-приложения
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "Welcome to Nebula Vape!", {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: "VAPE SHOP",
+                        web_app: { url: 'http://localhost:3001/shop.html' }
+                    }
+                ]
+            ]
+        }
+    });
+});
